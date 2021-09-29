258bf7e2d2cf6830eec7a7d085ff2b1c07a7d2f8
Sajed Malayeripour <sajedmalayeripour@hotmail.com>
2021-09-29 22:38:36 +0330

    Basket API and Entities init

---
+++ Microservices/Services/Basket/BasketAPI/Entities/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿namespace BasketAPI.Entities
+{
+    public class ShoppingCart
+    {
+        //I think it is in-memory data and it dont have to have ID
+        public string UserName { get; set; } = string.Empty;
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart() { }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName ?? string.Empty;
+        }
+        public decimal GetTotalPrice() => Items.Sum(i => i.Price * i.Count);
+    }
+}
---
+++ Microservices/Services/Basket/BasketAPI/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BasketAPI.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
